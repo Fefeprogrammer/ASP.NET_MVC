dcf25a2fd6fce56d26a2336cae63770363ce2a49
Fefeprogrammer <50031002+Fefeprogrammer@users.noreply.github.com>
2020-10-04 19:35:23 -0300

    Algumas alterações

---
+++ Business/Pagina.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Pagina
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Conteudo { get; set; }
+        public DateTime Data { get; set; }
+
+        public List<Pagina> Lista()
+        {
+            var paginaOb = new DataBase.Pagina();
+        }
+    }
+}
--- MVC/App_Start/RouteConfig.cs
+++ MVC/App_Start/RouteConfig.cs
@@ -14,6 +14,18 @@ namespace MVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "about",
+                "about",
+                defaults: new { controller = "Home", action = "Sobre" }
+            );
+
+            routes.MapRoute(
+                "contact",
+                "contact",
+                defaults: new { controller = "Home", action = "Contatos" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
